3374fe9194e5f0b0920c0725aa98f9a0229e2732
DanielODev <danielorlandi.dev@gmail.com>
2019-08-20 15:22:40 -0300

    Creating a main page

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Routes from './route'
+
+const App = () => <Routes />
+
+// class App extends Component {
+//     Render() {
+//         return <Routes />
+//     }
+// }
+
+export default App;
---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'React'
+
+import { View, Text} from 'react-native'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Página main</Text>
+            </View>
+        );
+    }
+}
